35e512f9ef6c2cfb097cd6c2a923b9a269f1e57b
Andre Geraseev <ageraseev@gmail.com>
2022-08-16 10:34:14 -0300

    make it better

--- usuario/models.py
+++ usuario/models.py
@@ -3,9 +3,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Usuario(AbstractUser):
-    username = models.CharField(max_length=50, unique=True, error_messages={'unique': "O usuario cadastrado já existe."})
-    password = models.CharField(max_length=8)
-    email = models.EmailField(max_length=254, unique=True, error_messages={'unique': "O email cadastrado já existe."})
+    username = models.CharField(max_length=40, unique=True, error_messages={'unique': "O usuario cadastrado já existe."})
+    password = models.CharField(max_length=6)
+    email = models.EmailField(max_length=30, unique=True, error_messages={'unique': "O email cadastrado já existe."})
 
 
     USERNAME_FIELD = 'username'
